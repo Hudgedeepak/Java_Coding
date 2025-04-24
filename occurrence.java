0026b1e103000a471e14046b04bbb6ce70bad134
Deepak Hudge <100028762+Hudgedeepak@users.noreply.github.com>
2025-04-24 12:11:26 +0530

    code to count the number of occurrences of a specific character in a string

---
+++ occurrence.java
@@ -0,0 +1,16 @@
+
+class CountCharacterOccurence
+{
+    public static void main(String[] args)
+    {
+        String s = "Java is java again java again";
+ 
+        char c = 'a';
+ 
+        int count = s.length() - s.replace("a", "").length();
+ 
+        System.out.println("Number of occurances of 'a' in "+s+" = "+count);
+    }
+}
+
+C:\Users\Deepak\Documents\code\Java_Coding
