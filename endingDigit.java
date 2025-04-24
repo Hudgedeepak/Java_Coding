5e3986b138da6b772dd6d6822c36d73f03bad5f3
Deepak Hudge <100028762+Hudgedeepak@users.noreply.github.com>
2025-04-24 12:21:38 +0530

    Added logic to print all numbers between 1 and 20000 that end with the digit 7

---
+++ endingDigit.java
@@ -0,0 +1,12 @@
+public class endingDigit {
+    public static void main(String[] args) {
+        int start = 1;
+  int end = 20000;
+  for(int i=start; i<end;i++)
+  {
+    if(i%10==7)
+    {System.out.println(i);}
+  }
+    }
+    
+}
