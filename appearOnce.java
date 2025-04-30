1e9f56b9a4420b392e8d4adf22f253b0ebc2a856
Deepak Hudge <100028762+Hudgedeepak@users.noreply.github.com>
2025-04-30 12:47:00 +0530

    Write a Java program to find and print the elements that appear only once in a sorted array

--- appearOnce.java
+++ appearOnce.java
@@ -2,10 +2,10 @@ import java.util.HashMap;
 
 public class appearOnce {
     public static void main(String[] args) {
-        int  a[]= {1,1,2,2,3,3,4,50,50,65,4,65,5};//find ele which one appear once
-		
-		// first approach
-		 HashMap<Integer, Integer> map = new HashMap<>();
+        int a[] = { 1, 1, 2, 2, 3, 3, 4, 50, 50, 65, 4, 65, 5 };// find ele which one appear once
+
+        // first approach 
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int num : a) {
             map.put(num, map.getOrDefault(num, 0) + 1);
@@ -17,11 +17,20 @@ public class appearOnce {
             }
         }
 
+        // second approach  
 
-		//second approach
+        for (int i = 0; i < a.length; i++) {
+            int count = 0;
 
+            for (int j = 0; j < a.length; j++) {
+                if (a[i] == a[j]) {
+                    count++;
+                }
+            }
+            if (count == 1){
+                System.out.print(a[i]);
+            }
+        }
 
-
-		
     }
 }
